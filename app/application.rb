25778b7d9e39804832bf676928106f59d41657d0
Brandon Leeds <brandonleeds@gmail.com>
2018-01-31 02:01:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,13 @@ class Application
   resp = Rack::Response.new
   middle_of_day = Time.parse "12:00 pm"
   current_time = Time.now
-  
+
+  if current_time < middle_of_day
+    resp.write "Good Morning"
+  else
+    resp.write "Good Afternoon"
+  end
+
   resp.finish
 
 end
